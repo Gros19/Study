e52f81835da7fc43db43ad46cf8502e7766167ab
Gros19 <2120110005@student.kopo.ac.kr>
2022-09-17 23:16:33 +0900

    kubernetes setting

---
+++ other_lesson/mac_k8s/config.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/env bash
+#Kubectl
+#swap 해제
+swapoff -a && sed -i '/swap/s/^/#/' /etc/fstab
+sudo modprobe br_netfilter
+# Set up required sysctl params, these persist across reboots.
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+modprobe br_netfilter
+
+sudo sysctl --system
+
+
+
+#호스트 
+sudo bash -c 'cat >> /etc/hosts << EOF
+192.168.1.10 m-k8s
+192.168.1.101 w1-k8s
+192.168.1.102 w2-k8s
+EOF
+'
+
+#name 서버 설정
+
+cat <<EOF > /etc/resolv.conf
+
+nameserver 1.1.1.1 #cloudflare DNS
+
+nameserver 8.8.8.8 #Google DNS
+
+EOF
+
+
+sudo ufw disable
---
+++ other_lesson/mac_k8s/install.sh
@@ -0,0 +1,86 @@
+ #! /usr/bin/env bash
+ #vim 설치
+ sudo apt-get install vim
+
+#도커 설치
+ sudo apt-get update
+
+ sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+	 sudo mkdir -p /etc/apt/keyrings
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
+sudo apt-get update
+
+apt-cache madison docker-ce
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo service docker start
+sudo systemctl enable --now docker
+
+sudo mkdir /etc/docker
+sudo cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+#crio install
+OS=xUbuntu_20.04
+VERSION=1.25
+
+# Add Kubic Repo
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" | tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+
+# Import Public Key
+sudo curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | apt-key add -
+
+# Add CRI Repo
+sudo echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /" | tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
+
+# Import Public Key
+sudo curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | apt-key add -
+
+
+sudo apt update
+sudo apt install cri-o cri-o-runc cri-tools -y
+
+sudo systemctl enable --now crio.service
+
+
+sudo systemctl start crio.service
+
+sudo crictl info
+
+
+
+sudo apt-get update
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+sudo systemctl daemon-reload
+sudo systemctl start kubelet && systemctl enable --now kubelet
+sudo systemctl restart kubelet
+
+sudo systemctl daemon-reload 
+sudo systemctl restart docker
